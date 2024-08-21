cd06fdbbfefb7c6b65a628e4ece8fa3b6e79b06f
Tristan A. <tristanagraa@gmail.com>
2024-08-22 04:47:00 +0700

    feat: add layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
+import { Toaster } from "../ui/sonner";
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <Navbar />
+      <Toaster position="top-center" />
+      <main className="min-h-screen w-[375px] bg-white-50 px-4">
+        {children}
+      </main>
+      <Footer />
+    </main>
+  );
+};
