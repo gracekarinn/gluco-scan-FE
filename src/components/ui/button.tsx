f0842bbc35a095e4cc43d97ae688514d68e6ba2d
gracekarinn <gracekarina12@gmail.com>
2024-08-22 13:11:24 +0700

    feat: update button component styles and variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,56 +1,55 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
-        default: "bg-zinc-900 text-zinc-50 hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
-        destructive:
-          "bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
-        outline:
-          "border border-zinc-200 bg-white hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
+        primary:
+          "bg-orange-500 text-orange-50 hover:bg-orange-400 hover:text-orange-50 disabled:bg-neutral-50 disabled:text-neutral-100 active:text-orange-50 active:bg-orange-700 focus:bg-orange-500 focus:text-orange-50 focus:border-orange-300",
         secondary:
-          "bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
-        ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
-        link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+          "bg-orange-50 text-orange-500 hover:bg-orange-50 hover:text-orange-500 active:bg-orange-300 active:text-orange-50 disabled:bg-neutral-50 disabled:text-neutral-100 disabled:cursor-not-allowed",
+        tertiary:
+          "text-orange-500 hover:bg-orange-50 hover:text-orange-500 disabled:bg-neutral-50 disabled:text-neutral-100 active:bg-orange-100 active:text-orange-700 focus:bg-neutral-50 focus:text-blue-500 focus:border-blue-300 focus:border-[2px]",
+        ghost:
+          "text-orange-500 border-[1px] border-orange-500  hover:bg-orange-50 hover:text-orange-500 disabled:bg-neutral-50 disabled:text-neutral-100 active:bg-orange-100 active:text-orange-700 focus:bg-neutral-50 focus:text-blue-500 focus:border-blue-300 focus:border-[2px]",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "h-10 px-4 py-2 rounded-xl",
+        sm: "h-[32px] rounded-xl px-4",
+        lg: "h-[48px] rounded-xl px-4",
+        icon: "h-[30px] w-[30px] rounded-xl",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
