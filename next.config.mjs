902b7c4a162be6e0589a3ce98406e3fd9e66f139
Tristan A. <tristanagraa@gmail.com>
2024-08-22 12:52:20 +0700

    feat: Add rewrite rule for API endpoint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () =>
+      process.env.NODE_ENV === 'production'
+        ? []
+        : [
+            {
+              source: '/api/:path*',
+              destination: 'http://localhost:3001/:path*',
+            },
+          ],
+  }
 
 export default nextConfig;
