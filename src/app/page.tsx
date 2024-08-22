927322881acd57724530db4fbfca75ebbc73929f
gracekarinn <gracekarina12@gmail.com>
2024-08-22 13:51:22 +0700

    chore: add checkbox component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
     <main className="mt-[30vh] flex flex-col gap-5">
       <Input type="text" placeholder="Enter your name" />
       <Button disabled>Submit</Button>
+      <Checkbox>Remember me</Checkbox>
     </main>
   );
 }
