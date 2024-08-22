38fef3baca3218d11b560f2949f5f9d15f8e3dae
gracekarinn <gracekarina12@gmail.com>
2024-08-22 13:11:13 +0700

    feat: add button component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="mt-[30vh]">
+    <main className="mt-[30vh] flex flex-col gap-5">
       <Input type="text" placeholder="Enter your name" />
+      <Button disabled>Submit</Button>
     </main>
   );
 }
