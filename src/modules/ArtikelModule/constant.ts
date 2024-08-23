39046f7c50f74a83380eec11c55f45647a87ae82
gracekarinn <gracekarina12@gmail.com>
2024-08-23 19:22:34 +0700

    feat: data for artikel

---
+++ src/modules/ArtikelModule/constant.ts
@@ -0,0 +1,34 @@
+export const ISI_ARTIKEL = [
+  {
+    id: 1,
+    img: "/berita-1.png",
+    time: "17 hours ago",
+    title: "Kasus diabetes anak meningkat ‘sangat mengkhawatirkan’,",
+    content:
+      "Makanan dan minuman manis begitu mudah dijangkau, sementara kebijakan pemerintah...",
+  },
+  {
+    id: 2,
+    img: "/berita-2.png",
+    time: "17 hours ago",
+    title: "Indonesia Peringkat Ke-5 dengan Kasus Diabetes Terbanyak",
+    content:
+      "Kasus penyakit diabetes Indonesia berada di angka yang sangat tinggi bila dibandingkan...",
+  },
+  {
+    id: 3,
+    img: "/berita-3.png",
+    time: "17 hours ago",
+    title: "Kasus Diabetes Meningkat, Kurangi Boba!",
+    content:
+      "Melihat kian meningkatnya kasus diabetes di Indonesia, pemerintah menghimbau warga...",
+  },
+  {
+    id: 4,
+    img: "/berita-4.png",
+    time: "17 hours ago",
+    title: "Ciri - Ciri Diabetes pada Anak di Bawah Umur",
+    content:
+      "Melihat kian meningkatnya kasus diabetes di Indonesia, pemerintah menghimbau warga...",
+  },
+];
