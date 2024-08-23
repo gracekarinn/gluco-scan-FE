25e8c19dee10ef6630ca257fbfa7acedd1d4db63
gracekarinn <gracekarina12@gmail.com>
2024-08-23 18:20:38 +0700

    feat: create home module

---
+++ src/modules/MainpageModule/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeSection from "./sections/HomeSection";
+
+const MainpageModule = () => {
+  return (
+    <div>
+      <HomeSection />
+    </div>
+  );
+};
+
+export default MainpageModule;
