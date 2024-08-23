db98ce1b926666810f9ad56d038f8006298b2591
gracekarinn <gracekarina12@gmail.com>
2024-08-23 18:20:57 +0700

    feat: unfinished home card

---
+++ src/modules/MainpageModule/components/HomeCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeCard = () => {
+  return (
+    <div className="bg-white border-2 border-orange-400 rounded-xl h-[140px]">
+      HomeCard
+    </div>
+  );
+};
+
+export default HomeCard;
