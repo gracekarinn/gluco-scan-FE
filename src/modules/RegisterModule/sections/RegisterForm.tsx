13862b942dad3fb970e41aba0171374fa377529f
gracekarinn <gracekarina12@gmail.com>
2024-08-27 23:15:19 +0700

    feat: add jeniskelamin component to registerform

--- src/modules/RegisterModule/sections/RegisterForm.tsx
+++ src/modules/RegisterModule/sections/RegisterForm.tsx
@@ -11,6 +11,7 @@ import {
 } from "../constant";
 import { EmailPass } from "../elements/EmailPass";
 import { TanggalLahir } from "../elements/TanggalLahir";
+import { JenisKelamin } from "../elements/JenisKelamin";
 
 export const RegisterForm = () => {
   const [page, setPage] = useState<number>(0);
@@ -26,12 +27,20 @@ export const RegisterForm = () => {
     console.log(data);
     setData(data);
     setPage(page + 1);
-  }
+  };
+
+  const onSubmitJenisKelamin = async (data: submitJenisKelaminData) => {
+    console.log(data);
+    setData(data);
+    setPage(page + 1);
+  };
 
   return (
     <section>
       {page === 0 && <TanggalLahir onSubmit={onSubmitTanggalLahir} />}
       {page === 1 && <div>{JSON.stringify(data)}</div>}
+      {page === 2 && <JenisKelamin onSubmit={onSubmitJenisKelamin} />}
+      {page === 3 && <div>{JSON.stringify(data)}</div>}
     </section>
   );
 };
