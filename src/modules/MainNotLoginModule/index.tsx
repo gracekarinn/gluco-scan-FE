cc4100da9b0d4b27f509a8b39e780591e8c087d2
gracekarinn <gracekarina12@gmail.com>
2024-08-28 14:21:12 +0700

    feat: update new sections

--- src/modules/MainNotLoginModule/index.tsx
+++ src/modules/MainNotLoginModule/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import MulaiSekarang from "./sections/MulaiSekarang";
+import CheckThisOut from "./sections/CheckThisOut";
+import FiturKami from "./sections/FiturKami";
 
 const MainNotLoginModule = () => {
-  return <div>MainNotLoginModule</div>;
+  return (
+    <div className="flex flex-col gap-y-20">
+      <MulaiSekarang />
+      <CheckThisOut />
+      <FiturKami />
+    </div>
+  );
 };
 
 export default MainNotLoginModule;
