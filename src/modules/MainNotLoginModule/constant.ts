a35d4a57fdd42098e38f27dfa2af868706605c73
gracekarinn <gracekarina12@gmail.com>
2024-08-28 14:21:01 +0700

    feat: add MENU_ITEMS constant for mainnotloginmodule

---
+++ src/modules/MainNotLoginModule/constant.ts
@@ -0,0 +1,29 @@
+import {
+  DocumentIcon,
+  ChatBubbleLeftIcon,
+  PresentationChartLineIcon,
+} from "@heroicons/react/16/solid";
+import { BarcodeIcon } from "lucide-react";
+
+export const MENU_ITEMS = [
+  {
+    icon: BarcodeIcon,
+    title: "Scan Produk",
+    url: "/scan",
+  },
+  {
+    icon: PresentationChartLineIcon,
+    title: "Monitor Asupan Gula",
+    url: "/monitor",
+  },
+  {
+    icon: ChatBubbleLeftIcon,
+    title: "Konsultasi Dokter",
+    url: "/konsultasi",
+  },
+  {
+    icon: DocumentIcon,
+    title: "Artikel Kesehatan",
+    url: "/artikel",
+  },
+];
