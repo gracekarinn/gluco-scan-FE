fb4d420a278246385b9b850e6adfaec5902e98f4
Tristan A. <tristanagraa@gmail.com>
2024-08-30 14:26:50 +0700

    feat: global footer

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,8 +6,8 @@ import { Toaster } from "../ui/sonner";
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen flex flex-col items-center bg-white-50">
-      <Navbar />
       <Toaster position="top-center" />
+      <Navbar />
       <main className="min-h-screen min-[320px]:w-[300px] min-[390px]:w-[375px] lg:w-[750px] px-4">
         {children}
       </main>
