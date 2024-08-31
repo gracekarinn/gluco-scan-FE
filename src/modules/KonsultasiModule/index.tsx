41952befc15fe976c564464a6c0e3da8602813d8
gracekarinn <gracekarina12@gmail.com>
2024-08-31 23:43:02 +0700

    feat: add konsultasimodule component

---
+++ src/modules/KonsultasiModule/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CariDokter } from "./sections/CariDokter";
+
+export const KonsultasiModule = () => {
+  return (
+    <div>
+      <CariDokter />
+    </div>
+  );
+};
