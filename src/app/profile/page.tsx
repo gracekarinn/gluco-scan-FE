5dc4638ac2caabee8cdf229f8bdb0f7dd6012b0a
Tristan A. <tristanagraa@gmail.com>
2024-09-01 05:51:08 +0700

    feat: profile module boilerplate

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ProfileModule } from "@/modules/ProfileModule";
+
+const ProfilePage = () => {
+  return <ProfileModule />;
+};
+
+export default ProfilePage;
---
+++ src/modules/ProfileModule/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProfileModule = () => {
+  return (
+    <main>index</main>
+  )
+}
