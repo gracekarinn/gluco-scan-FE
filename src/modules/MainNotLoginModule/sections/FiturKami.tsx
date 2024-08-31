e4dfc6a4f7058cae12a45a74073a4a5505345b98
gracekarinn <gracekarina12@gmail.com>
2024-08-31 23:43:33 +0700

    feat: add lazy loading for images in fiturkami section

--- src/modules/MainNotLoginModule/sections/FiturKami.tsx
+++ src/modules/MainNotLoginModule/sections/FiturKami.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MENU_ITEMS } from "../constant";
+import Link from "next/link";
 
 const FiturKami = () => {
   return (
@@ -7,13 +8,15 @@ const FiturKami = () => {
       <h1 className="font-semibold text-2xl">Fitur Kami</h1>
       <div className="flex flex-col gap-y-3 w-full h-[149px]">
         {MENU_ITEMS.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center gap-y-2 p-4 bg-orange-50 rounded-2xl"
-          >
-            <item.icon className="w-12 h-12 text-orange-500" />
-            <span className="font-semibold ">{item.title}</span>
-          </div>
+          <Link key={index} href={item.url}>
+            <div
+              key={index}
+              className="flex flex-col items-center gap-y-2 p-4 bg-orange-50 rounded-2xl"
+            >
+              <item.icon className="w-12 h-12 text-orange-500" />
+              <span className="font-semibold ">{item.title}</span>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
