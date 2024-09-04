2933cb2f6710aec63d627a27020237beb0846536
Tristan A. <tristanagraa@gmail.com>
2024-09-04 14:21:37 +0700

    refactor: Update QR scanner component and dependencies

--- src/modules/MainNotLoginModule/index.tsx
+++ src/modules/MainNotLoginModule/index.tsx
@@ -4,20 +4,63 @@ import MulaiSekarang from "./sections/MulaiSekarang";
 import CheckThisOut from "./sections/CheckThisOut";
 import FiturKami from "./sections/FiturKami";
 import { PaketLangganan } from "./sections/PaketLangganan";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import {
+  Scanner as ScannerComp,
+  IScannerProps,
+  outline,
+  boundingBox,
+  centerText,
+  useDevices,
+} from "@yudiel/react-qr-scanner";
 
 const MainNotLoginModule = () => {
   const [data, setData] = useState<any>("Not Found");
+  const [tracker, setTracker] = useState<string | undefined>("centerText");
+
 
   return (
     <div className="flex flex-col gap-y-20">
       <MulaiSekarang />
-      <Scanner
-        onScan={(result) => {
-          console.log(result);
-          setData(result);
+      <ScannerComp
+        formats={[
+          "qr_code",
+          "micro_qr_code",
+          "rm_qr_code",
+          "maxi_code",
+          "pdf417",
+          "aztec",
+          "data_matrix",
+          "matrix_codes",
+          "dx_film_edge",
+          "databar",
+          "databar_expanded",
+          "codabar",
+          "code_39",
+          "code_93",
+          "code_128",
+          "ean_8",
+          "ean_13",
+          "itf",
+          "linear_codes",
+          "upc_a",
+          "upc_e",
+        ]}
+        onScan={(detectedCodes) => {
+          console.log(`onScan: ${detectedCodes}`);
+          setData(detectedCodes);
         }}
+        onError={(error) => {
+          console.log(`onError: ${error}'`);
+        }}
+        components={{
+          audio: true,
+          onOff: true,
+          torch: true,
+          zoom: true,
+          finder: true,
+        }}
+        allowMultiple={true}
+        scanDelay={2000}
       />
       <p>{data}</p>
       <CheckThisOut />
