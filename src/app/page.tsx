fcc197456368a50b45141efd0db0193863b9184a
gracekarinn <gracekarina12@gmail.com>
2024-09-07 01:08:26 +0700

    feat: add footer component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import MainNotLoginModule from "@/modules/MainNotLoginModule";
+import { Footer } from "@/components/layout/Footer";
 
 export default function Home() {
-  return <MainNotLoginModule />;
+  return (
+    <main>
+      <MainNotLoginModule />
+      <Footer />
+    </main>
+  );
 }
