eb254db81f43454a6cc25fe3af9120f58737d147
gracekarinn <gracekarina12@gmail.com>
2024-09-07 01:09:36 +0700

    refactor: update subscard component to accept children prop

--- src/modules/MainNotLoginModule/elements/SubsCard.tsx
+++ src/modules/MainNotLoginModule/elements/SubsCard.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Unlock, Check } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export const SubsCard = () => {
+interface SubsCardProps {
+  children: React.ReactNode;
+}
+
+export const SubsCard: React.FC<SubsCardProps> = ({ children }) => {
   return (
     <div className="w-full h-full px-4 py-3 flex flex-col shadow-xl bg-white rounded-[20px] relative">
       <div className="absolute right-4 text-[#e6f2fa] font-medium text-P6 bg-orange-500 py-1 px-2 rounded-[20px]">
@@ -17,43 +21,29 @@ export const SubsCard = () => {
         </h1>
         <div className="grid grid-cols-2">
           <div className="flex flex-col gap-2">
-            <h2>Akses Penuh</h2>
             <div className="flex flex-col gap-1">
               <p className="text-P6 font-medium flex items-center gap-2 justify-start">
                 <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
+                Analisis personal
               </p>
               <p className="text-P6 font-medium flex items-center gap-2 justify-start">
                 <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
+                Telekonsultasi
               </p>
               <p className="text-P6 font-medium flex items-center gap-2 justify-start">
                 <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
+                Rekomendasi konsumsi
               </p>
               <p className="text-P6 font-medium flex items-center gap-2 justify-start">
                 <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h2>Akses Penuh</h2>
-            <div className="flex flex-col gap-1">
-              <p className="text-P6 font-medium flex items-center gap-2 justify-start">
-                <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
-              </p>
-              <p className="text-P6 font-medium flex items-center gap-2 justify-start">
-                <Check height={12.5} width={12.5} className="text-orange-500" />{" "}
-                Lorem ipsum
+                Batasan Harian
               </p>
             </div>
           </div>
         </div>
         <div className="flex justify-between text-H5 font-medium">
           <h3>Harga</h3>
-          <h3>Rp60.000</h3>
+          <h3>{children}</h3>
         </div>
         <Button>Langganan Sekarang</Button>
       </div>
