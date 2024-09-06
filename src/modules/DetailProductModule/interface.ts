8a91437498477d8ad13e3f7d8a538d8dca7cc80d
Tristan A. <tristanagraa@gmail.com>
2024-09-06 20:30:44 +0700

    feat: Add ProductDetailProps interface

---
+++ src/modules/DetailProductModule/interface.ts
@@ -0,0 +1,11 @@
+export interface ProductDetailProps {
+    productId: string;
+    namaProduk: string;
+    kadarGula: number;
+    image: string;
+    takaran: string;
+    energyKcal: number;
+    proteins: number;
+    fats: number;
+    carbohydrates: number;
+}
