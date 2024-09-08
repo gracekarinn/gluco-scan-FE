361d5ad8f44a59b9607b5fe26693ac218190a2fc
gracekarinn <gracekarina12@gmail.com>
2024-09-08 13:18:17 +0700

    refactor: remove unnecessary text

--- src/modules/RegisterModule/elements/TanggalLahir.tsx
+++ src/modules/RegisterModule/elements/TanggalLahir.tsx
@@ -47,10 +47,6 @@ export const TanggalLahir = ({
             <h1 className="text-center font-bold text-grey-900 text-H3">
               Kapan kamu lahir?
             </h1>
-            <p className="text-P6 tracking-wide font-normal text-center text-grey-900 mt-4">
-              Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
-              Ipsum Lorem Ipsum Lorem Ipsum
-            </p>
           </div>
           <FormField
             control={form.control}
