639493466cc527469ab68ec17d15141399267125
Tristan A. <tristanagraa@gmail.com>
2024-09-21 18:09:20 +0700

    feat: youtube url

--- src/modules/MainNotLoginModule/sections/CheckThisOut.tsx
+++ src/modules/MainNotLoginModule/sections/CheckThisOut.tsx
@@ -5,12 +5,11 @@ const CheckThisOut = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-y-3">
       <h1 className="font-semibold justify-center">Check this out!</h1>
-      <div className="w-full h-[153px] bg-orange-50 flex flex-col rounded-2xl">
-        <PlayCircleIcon
-          width={60}
-          height={60}
-          className="mx-auto my-auto text-orange-500"
-        />
+      <div className="w-full h-[350px] bg-orange-50 flex flex-col rounded-2xl">
+        <iframe
+          className="w-full h-full rounded-2xl"
+          src="https://www.youtube.com/embed/SiNI2XZqAe8"
+        ></iframe>
       </div>
     </div>
   );
