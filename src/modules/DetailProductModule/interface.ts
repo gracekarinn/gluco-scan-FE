5e636d5619fbae353f83bda58c3ef0242e46bad9
Tristan A. <tristanagraa@gmail.com>
2024-09-22 17:05:14 +0700

    refactor: Update DetailProductModule interface

--- src/modules/DetailProductModule/interface.ts
+++ src/modules/DetailProductModule/interface.ts
@@ -1,11 +1,25 @@
+import z from "zod";
 export interface ProductDetailProps {
-    productId: string;
-    namaProduk: string;
-    kadarGula: number;
-    image: string;
-    takaran: string;
-    energyKcal: number;
-    proteins: number;
-    fats: number;
-    carbohydrates: number;
+  productId: string;
+  namaProduk: string;
+  kadarGula: number;
+  image: string;
+  takaran: string;
+  energyKcal: number;
+  proteins: number;
+  fats: number;
+  carbohydrates: number;
+  servingSize: string;
 }
+
+export const sajianSchema = z.object({
+  sajian: z.string().refine(
+    (v) => {
+      let n = Number(v);
+      return !isNaN(n) && v?.length > 0;
+    },
+    { message: "Invalid number" }
+  ),
+});
+
+export type submitSajianData = z.infer<typeof sajianSchema>;
